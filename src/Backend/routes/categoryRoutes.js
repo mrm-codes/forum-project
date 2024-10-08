2d9a613a5574f7ae614f842a88ac1814274c3c9f
mrm-codes <mrmatundulo@gmail.com>
2024-10-08 06:43:48 -0500

    updated category routes and user dashboard side nav bar css properties

--- src/Backend/routes/categoryRoutes.js
+++ src/Backend/routes/categoryRoutes.js
@@ -1,25 +1,12 @@
 const express = require('express');
-const { check } = require('express-validator');
-const { register, login } = require('../controllers/authController');
+const { createCategory, getCategories } = require('../controllers/categoryController')
+
 const router = express.Router();
 
-// Middleware to validate registration input
-const validateRegistration = [
-  check('username').notEmpty().withMessage('Username is required'),
-  check('email').isEmail().withMessage('Valid email is required'),
-  check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-];
+// Creating Categories
+router.post('/category', createCategory);
 
-// Middleware to validate login input
-const validateLogin = [
-  check('email').isEmail().withMessage('Valid email is required'),
-  check('password').notEmpty().withMessage('Password is required'),
-];
-
-// Route to register a new user
-router.post('/register', validateRegistration, register);
-
-// Route to log in a user
-router.post('/login', validateLogin, login);
+// Accessing all Categories
+router.get('/categories', getCategories);
 
 module.exports = router;
